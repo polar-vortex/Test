203e85b092c25b90e0d15cb73e865b1da37ba3a2
Polar Shock <f.postnov@gmail.com>
2018-04-01 18:59:08 +0800

    map

---
+++ Assets/Polar Shock/Navigate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(Input.GetAxis("Horizontal")*25*Time.deltaTime,0,Input.GetAxis("Vertical")*25*Time.deltaTime));
+		transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"),0,0));
+		transform.Rotate(new Vector3(0,Input.GetAxis("Mouse X"),0),Space.World);
+	}
+}
